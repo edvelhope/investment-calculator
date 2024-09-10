22a9d20f2a87470ba3b21b584538fddc05081c66
Edoardo <edoardoderosaa@gmail.com>
2024-09-10 22:00:52 +0200

    App completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Results from "./components/Results";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    intialInvestment: 10000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
@@ -24,7 +26,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
